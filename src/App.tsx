a3957c226d24dcae40a737f403e451d71ff27565
Joshua Asistio <salazarelijahjoshuaasistio@gmail.com>
2024-06-13 11:21:29 -1200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profileImage from "./images/avatar-jessica.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-[100vh] flex justify-center items-center bg-[#141414]">
+      <main className="bg-[#1f1f1f] text-center p-6 rounded-xl text-white">
+        <section className="mb-9">
+          <img
+            src={profileImage}
+            alt="jessica's profile"
+            className="rounded-[50%] h-auto w-[90px] mx-auto mb-8"
+          />
+
+          <h1 className="text-2xl font-bold mb-5">Jessica Randall</h1>
+
+          <address>
+            <p className="text-[#bddc40]">London, United Kingdom</p>
+          </address>
+        </section>
+
+        <p className="text-[#e3e3e3]">"Front-end developer and avid reader."</p>
+
+        <section>
+          <ul>
+            <li>
+              <a href="#">Github</a>
+            </li>
+            <li>
+              <a href="#">Frontend Mentor</a>
+            </li>
+            <li>
+              <a href="#">Linkedin</a>
+            </li>
+            <li>
+              <a href="#">Twitter</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
